a70e7bec1419f5fcf4d7c8281337ce85926badbb
hari <harish00201@gmail.com>
2021-06-22 00:25:28 +0530

    commit3

--- application.py
+++ application.py
@@ -111,8 +111,9 @@ def login_validate():
         if res:
             session[email] = user
             return redirect(url_for('home_return', email = email))
-        else :
-            return render_template('home.html' , email = 'no' , user = '' , status = 'n' , reset = '6')
+
+    return render_template('home.html' , email = 'no' , user = '' , status = 'n' , reset = '6')
+
 
 def sendTempPass(email,passd,name):
     try:
@@ -582,6 +583,23 @@ def sendBlogMail(email):
 
 def build_blog_mail(link,email):
     return "<div style=\"font-family:Helvetica,Arial,sans-serif;font-size:16px;margin:0;color:#0b0c0c\">\n" +                "\n" +                "<span style=\"display:none;font-size:1px;color:#fff;max-height:0\"></span>\n" +               "\n" +                "  <table role=\"presentation\" width=\"100%\" style=\"border-collapse:collapse;min-width:100%;width:100%!important\" cellpadding=\"0\" cellspacing=\"0\" border=\"0\">\n" +                "    <tbody><tr>\n" +                "      <td width=\"100%\" height=\"53\" bgcolor=\"#0b0c0c\">\n" +                "        \n" +                "        <table role=\"presentation\" width=\"100%\" style=\"border-collapse:collapse;max-width:580px\" cellpadding=\"0\" cellspacing=\"0\" border=\"0\" align=\"center\">\n" +                "          <tbody><tr>\n" +                "            <td width=\"70\" bgcolor=\"#0b0c0c\" valign=\"middle\">\n" +                "                <table role=\"presentation\" cellpadding=\"0\" cellspacing=\"0\" border=\"0\" style=\"border-collapse:collapse\">\n" +                "                  <tbody><tr>\n" +                "                    <td style=\"padding-left:10px\">\n" +                "                  \n" +                "                    </td>\n" +                "                    <td style=\"font-size:28px;line-height:1.315789474;Margin-top:4px;padding-left:10px\">\n" +                "                      <span style=\"font-family:Helvetica,Arial,sans-serif;font-weight:700;color:#ffffff;text-decoration:none;vertical-align:top;display:inline-block\">Thanks for uploaded</span>\n" +                "                    </td>\n" +                "                  </tr>\n" +                "                </tbody></table>\n" +                "              </a>\n" +                "            </td>\n" +                "          </tr>\n" +                "        </tbody></table>\n" +                "        \n" +                "      </td>\n" +            "    </tr>\n" +             "  </tbody></table>\n" +            "  <table role=\"presentation\" class=\"m_-6186904992287805515content\" align=\"center\" cellpadding=\"0\" cellspacing=\"0\" border=\"0\" style=\"border-collapse:collapse;max-width:580px;width:100%!important\" width=\"100%\">\n" +            "    <tbody><tr>\n" +             "      <td width=\"10\" height=\"10\" valign=\"middle\"></td>\n" +            "      <td>\n" +           "        \n" +             "                <table role=\"presentation\" width=\"100%\" cellpadding=\"0\" cellspacing=\"0\" border=\"0\" style=\"border-collapse:collapse\">\n" +            "                  <tbody><tr>\n" +             "                    <td bgcolor=\"#1D70B8\" width=\"100%\" height=\"10\"></td>\n" +            "                  </tr>\n" +            "                </tbody></table>\n" +            "        \n" +             "      </td>\n" +            "      <td width=\"10\" valign=\"middle\" height=\"10\"></td>\n" +            "    </tr>\n" +             "  </tbody></table>\n" +             "\n" +         "\n" +       "\n" +         "  <table role=\"presentation\" class=\"m_-6186904992287805515content\" align=\"center\" cellpadding=\"0\" cellspacing=\"0\" border=\"0\" style=\"border-collapse:collapse;max-width:580px;width:100%!important\" width=\"100%\">\n" +             "    <tbody><tr>\n" +           "      <td height=\"30\"><br></td>\n" +          "    </tr>\n" +          "    <tr>\n" +           "      <td width=\"10\" valign=\"middle\"><br></td>\n" +              "      <td style=\"font-family:Helvetica,Arial,sans-serif;font-size:19px;line-height:1.315789474;max-width:560px\">\n" +                "        \n" +                "            <p style=\"Margin:0 0 20px 0;font-size:19px;line-height:25px;color:#0b0c0c\">Hi " + email + ",</p><p style=\"Margin:0 0 20px 0;font-size:19px;line-height:25px;color:#0b0c0c\"> You are blog was uploaded.Thank you for uploaded.we will publish your blog soon. once we published that will add on your profile page. Please click on the below link to go home page : </p><blockquote style=\"Margin:0 0 20px 0;border-left:10px solid #b1b4b6;padding:15px 0 0.1px 15px;font-size:19px;line-height:25px\"><p style=\"Margin:0 0 20px 0;font-size:19px;line-height:25px;color:#0b0c0c\"> <a href=\"" + link + "\">Goto Home Page</a> </p></blockquote>\n <p>See you soon</p>" +                "        \n" +                "      </td>\n" +        "      <td width=\"10\" valign=\"middle\"><br></td>\n" +                "    </tr>\n" +                "    <tr>\n" +                "      <td height=\"30\"><br></td>\n" +               "    </tr>\n" +                "  </tbody></table><div class=\"yj6qo\"></div><div class=\"adL\">\n" +               "\n" +                "</div></div>";
+
+
+@application.errorhandler(404)
+def error_404():
+    return render_template("404.html"),404
+
+@application.errorhandler(403)
+def error_403():
+    return render_template("403.html"),403
+
+@application.errorhandler(410)
+def error_410():
+    return render_template("410.html"),410
+
+@application.errorhandler(500)
+def error_500():
+    return render_template("500.html"),500
 
 
 if __name__ == "__main__":
